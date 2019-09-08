2cb8904eecd223659711fcd49e53cd62c5a9f05d
Audreycious <porcupine4@gmail.com>
2019-09-08 18:47:57 -0500

    updated based on feedback

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const uuid = require('uuid/v4')
+const logger = require('./logger')
 const bookmarkRouter = require("./routes/bookmarkRouter")
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
 const app = express()
-
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
@@ -16,30 +15,24 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-  
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         logger.error(`Unauthorized request to path: ${req.path}`);
         return res.status(401).json({ error: 'Unauthorized request' })
     }
-    // move to the next middleware
     next()
-  })
+})
 
 app.use(bookmarkRouter)
-
-app.delete("/bookmarks/:id", (req, res) => {
-    
-});
 
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
-    } else {
+    } 
+    else {
         console.error(error)
         response = { message: error.message, error }
     }
--- src/routes/bookmarkRouter.js
+++ src/routes/bookmarkRouter.js
@@ -11,32 +11,32 @@ bookmarkRouter
         res.json(bookmarks)
     })
     .post(bodyParser, (req, res) => {
-        const { title, url, desc, rating } = req.body;
+        const { title, url, desc, rating } = req.body
         if (!title) {
             logger.error(`Title is required`);
             return res
                 .status(400)
-                .send(`Invalid data`)
+                .send(`Title is required`)
         }
         if (!url) {
-            logger.error(`URL is required`);
+            logger.error(`URL is required`)
             return res
             .status(400)
-            .send('Invalid data');
+            .send('URL is required')
         }
         if (url.length < 5) {
-            logger.error(`URL is not longer than 5 characters`);
+            logger.error(`URL is not longer than 5 characters`)
             return res
             .status(400)
-            .send('Invalid data');
+            .send('URL must be at least 5 characters')
         }
         if (rating < 1 || rating > 5) {
-            logger.error(`Rating not between 1 and 5`);
+            logger.error(`Rating not between 1 and 5`)
             return res
             .status(400)
-            .send('Invalid data');
+            .send('Rating must be between 1 and 5')
         }
-        const id = uuid();
+        const id = uuid()
 
         const bookmark = {
             id,
@@ -44,50 +44,45 @@ bookmarkRouter
             url,
             desc,
             rating
-        };
+        }
 
-        bookmarks.push(bookmark);
+        bookmarks.push(bookmark)
 
-        logger.info(`Card with id ${id} created`);
+        logger.info(`Card with id ${id} created`)
 
         res
             .status(201)
             .location(`http://localhost:8000/card/${id}`)
-            .json(bookmark);
+            .json(bookmark)
   })
 
 bookmarkRouter
     .route('/bookmarks/:id')
     .get((req, res) => {
         const { id } = req.params;
-        const bookmark = bookmarks.find(mark => mark.id == id);
+        const bookmark = bookmarks.find(mark => mark.id == id)
         if (!bookmark) {
-            logger.error(`Bookmark with id ${id} not found.`);
+            logger.error(`Bookmark with id ${id} not found.`)
             return res
                 .status(404)
-                .send('Bookmark not found');
-    }
-
-    res.json(bookmark);
+                .send('Bookmark not found')
+        }
+        res.json(bookmark)
     })
     .delete((req, res) => {
-        const { id } = req.params;
-        const bookmarkIndex = bookmarks.findIndex(mark => mark.id == id);
-
+        const { id } = req.params
+        const bookmarkIndex = bookmarks.findIndex(mark => mark.id == id)
         if (bookmarkIndex === -1) {
-            logger.error(`Bookmark with id ${id} not found.`);
+            logger.error(`Bookmark with id ${id} not found.`)
             return res
             .status(404)
-            .send('Not found');
+            .send('Not found')
         }
-
-        bookmarks.splice(bookmarkIndex, 1);
-
-        logger.info(`Card with id ${id} deleted.`);
-
+        bookmarks.splice(bookmarkIndex, 1)
+        logger.info(`Card with id ${id} deleted.`)
         res
             .status(204)
-            .end();
+            .end()
     })
 
 module.exports = bookmarkRouter
